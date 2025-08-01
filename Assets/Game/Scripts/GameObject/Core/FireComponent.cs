771abd542381b4dcc34b30a9dcc59d11b5a1d5ca
Andrei Raketa <raketa420311@gmail.com>
2025-08-01 21:28:07 +0300

    cooldown fire added

--- Assets/Game/Scripts/GameObject/Core/FireComponent.cs
+++ Assets/Game/Scripts/GameObject/Core/FireComponent.cs
@@ -9,14 +9,25 @@ public sealed class FireComponent : NetworkBehaviour
     [SerializeField]
     private NetworkPrefabRef _projectilePrefab;
     
+    [SerializeField]
+    private float _cooldown = 1;
+
+    [Networked]
+    private TickTimer _timer { get; set; }
+    
     public void Fire()
     {
         if (!Runner.IsServer)
+            return;
+
+        if (!_timer.ExpiredOrNotRunning(Runner))
             return;
         
         PlayerRef player = Object.InputAuthority;
         Vector3 position = _firePoint.position;
         Quaternion rotation = _firePoint.rotation;
         Runner.Spawn(_projectilePrefab, position, rotation, player);
+
+        _timer = TickTimer.CreateFromSeconds(Runner, _cooldown);
     }
 }
